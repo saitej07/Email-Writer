266e92566299d08d6d63c6fbf6be1afd576866bd
saitej07 <saitejabollabathini@gmail.com>
2025-01-15 22:20:24 +0530

    Email-writer spring boot

---
+++ src/main/java/com/saiteja/email_writer/EmailWriterSpApplication.java
@@ -0,0 +1,13 @@
+package com.saiteja.email_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriterSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterSpApplication.class, args);
+	}
+
+}
